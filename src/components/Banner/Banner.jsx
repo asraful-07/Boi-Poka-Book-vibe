4815c5e1f7cdc47680faeb146cf5da039acc7a46
asraful-07 <asraful0508@gmail.com>
2024-11-09 17:57:49 +0600

    added banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,12 +4,12 @@ import BannerIm from "../../assets/images/books.jpg";
 const Banner = () => {
   return (
     <div className="mb-24">
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="hero bg-base-200 mb-24 py-20">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={BannerIm} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
+          <img src={BannerIm} className="max-w-xl rounded-lg shadow-2xl" />
+          <div className="">
+            <h1 className="text-6xl font-bold">Box Office News!</h1>
+            <p className="py-6 w-3/4">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
